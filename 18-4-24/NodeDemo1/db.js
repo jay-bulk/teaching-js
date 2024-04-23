42d7688ef8a386b8a2327ae7bfb6b315ba86e339
rhettjay <jay.bulk@tuta.io>
2024-04-23 02:15:56 -0600

    chore(18-4-24): class fixes

--- 18-4-24/NodeDemo1/db.js
+++ 18-4-24/NodeDemo1/db.js
@@ -1,7 +1,7 @@
 import sql from 'mssql'
 
 const config = {
-    user: process.env.DB_USER ?? '', 
+    user: process.env.DB_USER ?? '',
     password: process.env.DB_PASSWORD ?? '',
     server: process.env.DB_HOST ?? '',
     database: process.env.DB_SCHEMA ?? '',
@@ -9,11 +9,11 @@ const config = {
 }
 
 sql.connect(config)
-.then(conn => {
-    return conn.query('Select * from film')
-})
-.then(data => {
-    console.log(data.recordset)
+    .then(conn => {
+        return conn.query('Select * from film')
+    })
+    .then(data => {
+        console.log(data.recordset)
 
-})
-.catch(err => {console.log(`${err}: something went wrong`)})
+    })
+    .catch(err => { console.log(`${err}: something went wrong`) })
