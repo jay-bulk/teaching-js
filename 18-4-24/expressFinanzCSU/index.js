9ae3ec34bca8c2516753286682cf197eb155cf0e
rhettjay <jay.bulk@tuta.io>
2024-04-25 21:27:41 -0600

    feat: jwt generation

--- 18-4-24/expressFinanzCSU/index.js
+++ 18-4-24/expressFinanzCSU/index.js
@@ -64,13 +64,74 @@ app.get('/transactions/:month_id', (req, res) => {
 app.post('/register', async (req, res) => {
   let fullName = req.body.fullName
   let uName = req.body.uName
-  let userPassword = req.body.userPassword
+  const userPassword = req.body.userPassword
 
   if (!fullName || !uName || !userPassword) {
     return res.status(400).send('Bad Request Missing Item')
   }
   fullName = fullName.replace("'", "''")
   uName = uName.replace("'", "''")
-  userPassword = userPassword.replace("'", "''")
   uRole = 'User'
+
+  const UNameCheckQuery = `SELECT UName from LoginInfo where UName = '${uName}'`
+
+  const duplicateUserName = await execQuery(UNameCheckQuery)
+  if (duplicateUserName[0]){
+      return res.status(400).send('Please enter a different username')
+  }
+
+  const hashedPassword = bcrypt.hashSync(userPassword)
+
+  const userInsert = `INSERT INTO LoginInfo(UName, UPass, FullName, URole)
+                        VALUES('${uName}','${hashedPassword}','${fullName}','${uRole}')`
+  await execQuery(userInsert)
+  .then(() => {
+    res.status(201).send('Registration Successful')
+  })
+  .catch((err) => {
+    console.error(err, 'Invalid Registration.')
+    return res.status(500).send('Registration Unsuccessful. Please try again later')
+  })
+})
+
+app.post('/login', async (req, res) => {
+
+  const uName = req.body.uName
+  const userPassword = req.body.userPassword
+
+  const UNameCheckQuery = `SELECT UName from NodeLoginInfo where UName = '${uName}'`
+  let result;
+
+  try {
+    result = await execQuery(UNameCheckQuery)
+  } catch (err) {
+    console.error('Login error', err)
+    return res.status(500).send()
+  }
+  const user = result[0]
+
+  if (!bcrypt.compareSync(userPassword, user.UPass)) {
+    return res.status(401).send('Invalid Credentials')
+  }
+
+  const token = jwt.sign({pk: user.UserID}, process.env.JWT_PRIVATE_KEY, {expiresIn: '30 minutes'})
+
+  const setTokenQuery = `UPDATE NodeLoginInfo 
+                        SET Token = '${token}' WHERE UserID = '${user.UserID}'`
+
+  try {
+    await execQuery(setTokenQuery)
+
+    res.status(200).send({
+      'jwt-assertion': token,
+      contact: {
+          'Full Name': user.FullName,
+        Username: user.UName,
+        UserID: user.UserID
+      }
+    })
+  } catch (err) {
+    console.error(err, 'Failed to get token')
+    res.status(500).send()
+  }
 })
