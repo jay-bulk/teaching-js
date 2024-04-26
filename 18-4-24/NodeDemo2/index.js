a76c1d76d0ce74d583abd07f2907b211ab4677de
rhettjay <jay.bulk@tuta.io>
2024-04-25 21:28:22 -0600

    feat: encryption

--- 18-4-24/NodeDemo2/index.js
+++ 18-4-24/NodeDemo2/index.js
@@ -1,10 +1,12 @@
 // Author: Rhett Bulkley
 import express from 'express'
 import { execQuery } from './dbConnExec.js'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
  
 const app = express()
 
-app.user(express.json())
+app.use(express.json())
 const PORT = 1340
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`)
@@ -61,14 +63,73 @@ app.get('/films/:id', (req, res) => {
 app.post('/register', async (req, res) => {
   let firstName = req.body.firstName
   let lastName = req.body.lastName
-  let email = req.body.email
-  let userPassword = req.body.userPassword
+  const email = req.body.email
+  const userPassword = req.body.userPassword
 
   if (!firstName || !lastName || !email || !userPassword) {
     return res.status(400).send('Bad Request Missing Item')
   }
   firstName = firstName.replace("'", "''")
   lastName = lastName.replace("'", "''")
-  email = email.replace("'", "''")
 
+  const emailCheckQuery = `Select email from contact where email = '${email}'`;
+
+  const duplicateEmail = await execQuery(emailCheckQuery)
+  if (duplicateEmail[0]) {
+    return res.status(400).send('Please enter a different email')
+  }
+
+  const hashedPassword = bcrypt.hashSync(userPassword)
+
+  const userInsert = `INSERT INTO contact(FirstName, LastName, Email, UserPassword) 
+                        VALUES('${firstName}', '${lastName}', '${email}', '${hashedPassword}')`
+  await execQuery(userInsert)
+  .then(() => res.status(201).send('Registration Successful'))
+  .catch((err) =>  {
+    console.error(err)
+    return res.status(500).send('Registration Unsuccessful. Please try again later')
+  })
+})
+
+app.post('/login', async (req, res) => {
+
+  const email = req.body.email
+  const userPassword = req.body.userPassword
+  const emailCheckQuery = `Select * FROM contact where email = '${email}'`
+
+  let result;
+
+  try {
+    result = await execQuery(emailCheckQuery)
+  } catch (err) {
+    console.error('Login error', err)
+    return res.status(500).send()
+  }
+  const contact = result[0]
+
+  if (!bcrypt.compareSync(userPassword, contact.UserPassword)) {
+    return res.status(401).send('Invalid Credentials')
+  }
+
+  const token = jwt.sign({pk: contact.ContactPK}, process.env.JWT_PRIVATE_KEY, {expiresIn: '30 minutes'})
+
+  let setTokenQuery = `UPDATE contact
+                        SET token = '${token}' WHERE ContactPK = '${contact.ContactPK}'`
+
+  try {
+    await execQuery(setTokenQuery)
+
+    res.status(200).send({
+      token: token,
+      contact: {
+        FirstName: contact.FirstName,
+        LastName: contact.LastName,
+        Email: contact.Email,
+        ContactPK: contact.ContactPK
+      }
+    })
+  } catch (err) {
+    console.error(err, 'Failed to get token')
+    res.status(500).send()
+  }
 })
